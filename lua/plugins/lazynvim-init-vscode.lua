8b83ed3411cdeb5bc9df5cd8ef7495ff8f59a541
Elflare <271374942@qq.com>
2024-11-13 12:29:51 +0800

    add multicursors.nvim

--- lua/plugins/lazynvim-init-vscode.lua
+++ lua/plugins/lazynvim-init-vscode.lua
@@ -15,6 +15,24 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(
     {
         "numToStr/Comment.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        {
+            "smoka7/multicursors.nvim",
+            event = "VeryLazy",
+            dependencies = {
+                "nvimtools/hydra.nvim"
+            },
+            opts = {},
+            cmd = {"MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor"},
+            keys = {
+                {
+                    mode = {"v", "n"},
+                    "<Leader>m",
+                    "<cmd>MCstart<cr>",
+                    desc = "Create a selection for selected text or word under the cursor"
+                }
+            }
+        },
         "nguyenvukhang/nvim-toggler",
         {
             "folke/flash.nvim",
@@ -30,6 +48,14 @@ require("lazy").setup(
                         require("flash").jump()
                     end,
                     desc = "Flash"
+                },
+                {
+                    "?",
+                    mode = {"n", "x", "o"},
+                    function()
+                        require("flash").treesitter()
+                    end,
+                    desc = "Flash Treesitter"
                 },
                 {
                     "r",
