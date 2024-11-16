799aecb7594d22283863f11f468ad7352f68f41b
Elflare <271374942@qq.com>
2024-11-16 21:43:05 +0800

    remove multicursors for vscode

--- lua/plugins/lazynvim-init-vscode.lua
+++ lua/plugins/lazynvim-init-vscode.lua
@@ -18,23 +18,6 @@ require("lazy").setup(
         "nvim-treesitter/nvim-treesitter",
         "nguyenvukhang/nvim-toggler",
         {
-            "smoka7/multicursors.nvim",
-            event = "VeryLazy",
-            dependencies = {
-                "nvimtools/hydra.nvim"
-            },
-            opts = {},
-            cmd = {"MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor"},
-            keys = {
-                {
-                    mode = {"v", "n"},
-                    "<Leader>m",
-                    "<cmd>MCstart<cr>",
-                    desc = "Create a selection for selected text or word under the cursor"
-                }
-            }
-        },
-        {
             "folke/flash.nvim",
             event = "VeryLazy",
             ---@type Flash.Config
