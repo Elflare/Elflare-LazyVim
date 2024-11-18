cd1a4b351d0cadfe07447eaf2bab991cff2c066d
Elflare <271374942@qq.com>
2024-11-18 14:56:26 +0800

    remove duplicate multicursors

--- lua/plugins/lazynvim-init.lua
+++ lua/plugins/lazynvim-init.lua
@@ -89,23 +89,6 @@ require("lazy").setup(
             }
         },
         {
-            "smoka7/multicursors.nvim",
-            event = "VeryLazy",
-            dependencies = {
-                "nvimtools/hydra.nvim"
-            },
-            opts = {},
-            cmd = {"MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor"},
-            keys = {
-                {
-                    mode = {"v", "n"},
-                    "<Leader>m",
-                    "<cmd>MCstart<cr>",
-                    desc = "Create a selection for selected text or word under the cursor"
-                }
-            }
-        },
-        {
             "folke/which-key.nvim",
             event = "VeryLazy",
             opts = {},
