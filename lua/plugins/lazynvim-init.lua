f76e41e7fc4cff95b1ee830f494cba3c897239aa
Elflare <271374942@qq.com>
2025-01-14 10:45:53 +0800

    update

--- lua/plugins/lazynvim-init.lua
+++ lua/plugins/lazynvim-init.lua
@@ -1,238 +1,234 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system(
-        {
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "https://github.com/folke/lazy.nvim.git",
-            "--branch=stable", -- latest stable release
-            lazypath
-        }
-    )
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(
-    {
-        {
-            "stevearc/oil.nvim",
-            opts = {},
-            -- Optional dependencies
-            dependencies = {"nvim-tree/nvim-web-devicons"}
-        },
-        "neovim/nvim-lspconfig",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "onsails/lspkind-nvim",
-        "yehuohan/cmp-im",
-        "yehuohan/cmp-im-zh",
-        "nvim-treesitter/nvim-treesitter",
-        -- "hrsh7th/cmp-vsnip",
-        -- "hrsh7th/vim-vsnip",
-        "numToStr/Comment.nvim",
-        "windwp/nvim-autopairs",
-        "petertriho/nvim-scrollbar",
-        "EdenEast/nightfox.nvim",
-        "simnalamburt/vim-mundo",
-        "nguyenvukhang/nvim-toggler",
-        "brenoprata10/nvim-highlight-colors",
-        {
-            "lukas-reineke/indent-blankline.nvim",
-            main = "ibl",
-            ---@module "ibl"
-            ---@type ibl.config
-            opts = {}
-        },
-        {
-            {
-                "supermaven-inc/supermaven-nvim",
-                config = function()
-                    require("supermaven-nvim").setup(
-                        {
-                            keymaps = {
-                                accept_suggestion = "<C-CR>",
-                                clear_suggestion = "<C-]>",
-                                accept_word = "<C-Right>"
-                            }
-                        }
-                    )
-                end
-            }
-        },
-        -- lazygit
-        {
-            "kdheepak/lazygit.nvim",
-            lazy = true,
-            cmd = {
-                "LazyGit",
-                "LazyGitConfig",
-                "LazyGitCurrentFile",
-                "LazyGitFilter",
-                "LazyGitFilterCurrentFile"
-            },
-            -- optional for floating window border decoration
-            dependencies = {
-                "nvim-lua/plenary.nvim"
-            },
-            -- setting the keybinding for LazyGit with 'keys' is recommended in
-            -- order to load the plugin when the command is run for the first time
-            keys = {
-                {"<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit"}
-            }
-        },
-        -- {
-        --     "folke/noice.nvim",
-        --     event = "VeryLazy",
-        --     opts = {},
-        --     dependencies = {
-        --         --   `nvim-notify` is only needed, if you want to use the notification view.
-        --         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        --         "MunifTanjim/nui.nvim",
-        --         -- "rcarriga/nvim-notify"
-        --     }
-        -- },
-        -- lazy.nvim:
-        {
-            "smoka7/multicursors.nvim",
-            event = "VeryLazy",
-            dependencies = {
-                "nvimtools/hydra.nvim"
-            },
-            opts = {},
-            cmd = {"MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor"},
-            keys = {
-                {
-                    mode = {"v", "n"},
-                    "<Leader>m",
-                    "<cmd>MCstart<cr>",
-                    desc = "Create a selection for selected text or word under the cursor"
-                }
-            }
-        },
-        {
-            "folke/which-key.nvim",
-            event = "VeryLazy",
-            opts = {},
-            keys = {
-                {
-                    "<leader>?",
-                    function()
-                        require("which-key").show({global = false})
-                    end,
-                    desc = "Buffer Local Keymaps (which-key)"
-                }
-            }
-        },
-        {
-            "iamcco/markdown-preview.nvim",
-            cmd = {"MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop"},
-            ft = {"markdown"},
-            build = function()
-                vim.fn["mkdp#util#install"]()
-            end
-        },
-        {
-            "smjonas/inc-rename.nvim",
-            config = function()
-                require("inc_rename").setup()
-            end
-        },
-        {
-            "goolord/alpha-nvim",
-            dependencies = {"nvim-tree/nvim-web-devicons"},
-            config = function()
-                require "alpha".setup(require "alpha.themes.startify".config)
-            end
-        },
-        {
-            "Pocco81/auto-save.nvim",
-            config = function()
-                require("auto-save").setup {}
-            end
-        },
-        "nvim-tree/nvim-tree.lua",
-        "christoomey/vim-tmux-navigator",
-        -- "sbdchd/neoformat",
-        -- {
-        --     "nvim-telescope/telescope.nvim",
-        --     dependencies = {"nvim-lua/plenary.nvim"}
-        -- },
-        {
-            "stevearc/conform.nvim",
-            event = "VeryLazy"
-        },
-        "nvim-telescope/telescope.nvim",
-        "lewis6991/gitsigns.nvim",
-        {
-            "akinsho/bufferline.nvim",
-            version = "*",
-            dependencies = "nvim-tree/nvim-web-devicons"
-        },
-        {
-            "folke/flash.nvim",
-            event = "VeryLazy",
-            ---@type Flash.Config
-            opts = {},
-            -- stylua: ignore}
-            keys = {
-                {
-                    "Q",
-                    mode = {"n", "x", "o"},
-                    function()
-                        require("flash").jump()
-                    end,
-                    desc = "Flash"
-                },
-                {
-                    "?",
-                    mode = {"n", "x", "o"},
-                    function()
-                        require("flash").treesitter()
-                    end,
-                    desc = "Flash Treesitter"
-                },
-                {
-                    "r",
-                    mode = "o",
-                    function()
-                        require("flash").remote()
-                    end,
-                    desc = "Remote Flash"
-                },
-                {
-                    "R",
-                    mode = {"o", "x"},
-                    function()
-                        require("flash").treesitter_search()
-                    end,
-                    desc = "Treesitter Search"
-                },
-                {
-                    "<c-s>",
-                    mode = {"c"},
-                    function()
-                        require("flash").toggle()
-                    end,
-                    desc = "Toggle Flash Search"
-                }
-            }
-        },
-        {
-            "kylechui/nvim-surround",
-            version = "*", -- Use for stability; omit to use `main` branch for the latest features
-            event = "VeryLazy",
-            config = function()
-                require("nvim-surround").setup({})
-            end
-        },
-        {
-            "nvim-lualine/lualine.nvim",
-            dependencies = {"nvim-tree/nvim-web-devicons"}
-        }
-    }
-)
+require("lazy").setup({
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	"neovim/nvim-lspconfig",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/nvim-cmp",
+	"onsails/lspkind-nvim",
+	"yehuohan/cmp-im",
+	"yehuohan/cmp-im-zh",
+	"nvim-treesitter/nvim-treesitter",
+	-- "hrsh7th/cmp-vsnip",
+	-- "hrsh7th/vim-vsnip",
+	"numToStr/Comment.nvim",
+	"windwp/nvim-autopairs",
+	"petertriho/nvim-scrollbar",
+	"EdenEast/nightfox.nvim",
+	"simnalamburt/vim-mundo",
+	"nguyenvukhang/nvim-toggler",
+	"brenoprata10/nvim-highlight-colors",
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false, -- Recommended
+		-- ft = "markdown" -- If you decide to lazy-load anyway
+
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
+	{
+		{
+			"supermaven-inc/supermaven-nvim",
+			config = function()
+				require("supermaven-nvim").setup({
+					keymaps = {
+						accept_suggestion = "<C-CR>",
+						clear_suggestion = "<C-]>",
+						accept_word = "<C-Right>",
+					},
+				})
+			end,
+		},
+	},
+	-- lazygit
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+	-- {
+	--     "folke/noice.nvim",
+	--     event = "VeryLazy",
+	--     opts = {},
+	--     dependencies = {
+	--         --   `nvim-notify` is only needed, if you want to use the notification view.
+	--         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+	--         "MunifTanjim/nui.nvim",
+	--         -- "rcarriga/nvim-notify"
+	--     }
+	-- },
+	-- lazy.nvim:
+	{
+		"smoka7/multicursors.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvimtools/hydra.nvim",
+		},
+		opts = {},
+		cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+		keys = {
+			{
+				mode = { "v", "n" },
+				"<Leader>m",
+				"<cmd>MCstart<cr>",
+				desc = "Create a selection for selected text or word under the cursor",
+			},
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"smjonas/inc-rename.nvim",
+		config = function()
+			require("inc_rename").setup()
+		end,
+	},
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"Pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup({})
+		end,
+	},
+	"nvim-tree/nvim-tree.lua",
+	"christoomey/vim-tmux-navigator",
+	-- "sbdchd/neoformat",
+	-- {
+	--     "nvim-telescope/telescope.nvim",
+	--     dependencies = {"nvim-lua/plenary.nvim"}
+	-- },
+	{
+		"stevearc/conform.nvim",
+		event = "VeryLazy",
+	},
+	"nvim-telescope/telescope.nvim",
+	"lewis6991/gitsigns.nvim",
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		-- stylua: ignore}
+		keys = {
+			{
+				"Q",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"?",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+})
 local Flash = require("flash")
 Flash.toggle()
